bb85c823018e22fa9cfdb1e348c853669f7187de
Joseph Wiel <joseph@comet.co>
2019-02-19 14:50:00 +0100

    added the root list and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    select_task
+  end
+
+  private
+
+  def select_task
+    @task = Task.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake tasks'
+100.times do
+  Task.create(
+    title: Faker::Space.planet,
+    details: Faker::Space.agency,
+    completed: [true, false].sample
+  )
+end
+puts 'Done seeding the database.'

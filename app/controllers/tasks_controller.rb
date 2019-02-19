b7f3eb84635fa2090079120acc1998b5c4d45d39
Joseph Wiel <joseph@comet.co>
2019-02-19 17:03:06 +0100

    ended the task management app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,37 @@ class TasksController < ApplicationController
     select_task
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task_params
+    Task.create(task_params)
+    redirect_to '/tasks'
+  end
+
+  def edit
+    select_task
+  end
+
+  def update
+    select_task
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    select_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 
   def select_task
     @task = Task.find(params[:id])
